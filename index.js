35e843d179da903f5f7c8925259b573bb936de5b
Miliya Immatty Lazar <miliyaninu@gmail.com>
2024-09-20 15:30:55 +1000

    completed

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function introduction(name) {
-    return `Hi, my name is ${name}`
-  }
+  return `Hi, my name is ${name}.`;
+}
   introduction("Aki")
+
+
 function introductionWithLanguage(name,language){
-    console.log(`my name is ${name} and i am learning to program in ${language}`)
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 introductionWithLanguage("Aki","Ember.js")
+
+
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    console.log(`my name is ${name} and i am learning to program in ${language}`);
-  }
-  introductionWithLanguageOptional("Aki");
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+  introductionWithLanguageOptional('Gracie');
+  
+  
+  
   
